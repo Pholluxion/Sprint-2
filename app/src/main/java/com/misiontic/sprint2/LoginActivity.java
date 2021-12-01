910c959eaca2b75177007133a17bed1f19d9b546
Pholluxion <penalozadaniel00@gmail.com>
2021-11-30 21:50:41 -0500

    Add Home Activity and Register Activity

--- app/src/main/java/com/misiontic/sprint2/LoginActivity.java
+++ app/src/main/java/com/misiontic/sprint2/LoginActivity.java
@@ -1,8 +1,14 @@
 package com.misiontic.sprint2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +16,46 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView goToRegister = findViewById(R.id.goToReg);
+        goToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToReg = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(goToReg);
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            closeApplication();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void closeApplication() {
+
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.ic_baseline_exit_to_app)
+                .setTitle("¿Realmente desea cerrar la aplicación?")
+                .setCancelable(false)
+                .setNegativeButton(android.R.string.cancel, null)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {// un listener que al pulsar, cierre la aplicacion
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        android.os.Process.killProcess(android.os.Process.myPid()); //Su funcion es algo similar a lo que se llama cuando se presiona el botón "Forzar Detención" o "Administrar aplicaciones", lo cuál mata la aplicación
+                        //finish(); Si solo quiere mandar la aplicación a segundo plano
+                    }
+                }).show();
+
+
+
     }
 }
